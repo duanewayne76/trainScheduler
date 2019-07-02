6607f69d9427f6ff59d4412f7f7abc44fcf18447
Duane Stafford <duanestafford@Duanes-MBP.fios-router.home>
2019-07-02 00:17:23 -0400

    firebase added

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
-//Initialize Firebase
-// var config = {
-//     apiKey: "AIzaSyDVDNY6tIIFJtYILw_fIhrQ6GyHsepMx_c",
-//     authDomain: "traintime-e5269.firebaseapp.com",
-//     databaseURL: "https://traintime-e5269.firebaseio.com",
-//     storageBucket: "traintime-e5269.appspot.com"
-//   };
-
-//   firebase.initializeApp(config);
-
-//   var database = firebase.database();
-
-$('#s').on("click", function (event) {
-
-    event.preventDefault();
-
-    var name = $('#newName').val().trim();
-    var destination = $('#newDestination').val().trim();
-    var time = $('#newTime').val().trim();
-    var frequency = $('#newFrequency').val().trim();
-
-    var addedTrain = {
-        name: name,
-        destination: destination,
-        time: time,
-        frequency: frequency,
-    }
-    console.log(addedTrain);
-    database.ref().push(addedTrain);
+$(document).ready(function () {
 
 
+    //Initialize Firebase
+    // var config = {
+    //     apiKey: "AIzaSyDVDNY6tIIFJtYILw_fIhrQ6GyHsepMx_c",
+    //     authDomain: "traintime-e5269.firebaseapp.com",
+    //     databaseURL: "https://traintime-e5269.firebaseio.com",
+    //     storageBucket: "traintime-e5269.appspot.com"
+    //   };
+
+    //   firebase.initializeApp(config);
+
+    //   var database = firebase.database();
+
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDVDNY6tIIFJtYILw_fIhrQ6GyHsepMx_c",
+        authDomain: "traintime-e5269.firebaseapp.com",
+        databaseURL: "https://traintime-e5269.firebaseio.com",
+        projectId: "traintime-e5269",
+        storageBucket: "traintime-e5269.appspot.com",
+        messagingSenderId: "186117666666",
+        appId: "1:186117666666:web:fbf9da78e3db5dae"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database();
+
+    $('#s').on("click", function (event) {
+
+        event.preventDefault();
+
+        var name = $('#newName').val().trim();
+        var destination = $('#newDestination').val().trim();
+        var time = $('#newTime').val().trim();
+        var frequency = $('#newFrequency').val().trim();
+
+        var addedTrain = {
+            name: name,
+            destination: destination,
+            time: time,
+            frequency: frequency,
+        }
+        console.log(addedTrain);
+        database.ref().push(addedTrain);
+
+
+
+    });
     database.ref().on("child_added", function (childSnapshot) {
 
         // Log everything that's coming out of snapshot
@@ -38,11 +58,11 @@ $('#s').on("click", function (event) {
         console.log(childSnapshot.val().frequency);
 
         var x = $('<tr>');
-        x.html(`<th scope="row">${childSnapshot.val().name}</th><td>${childSnapshot.val().destination}</td><td>${childSnapshot.val().time}</td><td>months</td><td>${childSnapshot.val().frequency}</td><td>billHours</td>`);
+        x.html(`<th scope="row">${childSnapshot.val().name}</th><td>${childSnapshot.val().destination}</td><td>${childSnapshot.val().frequency}</td><td>${childSnapshot.val().time}</td>`);
         $('#schedule-table').append(x);
 
         // Handle the errors
     }, function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
     });
-});
+})
