33787bd56d27d8abe48520a6d50f9488fa5d30df
Duane Stafford <duanestafford@Duanes-MacBook-Pro.local>
2019-07-08 22:31:21 -0400

    fixed moment.js

--- app.js
+++ app.js
@@ -37,41 +37,63 @@ $(document).ready(function () {
         var destination = $('#newDestination').val().trim();
         var time = $('#newTime').val().trim();
         var frequency = $('#newFrequency').val().trim();
-        var minutesAway = moment().subtract(time).format ("hh:mm");
-        var currentTime = moment();
 
         var addedTrain = {
             name: name,
             destination: destination,
             frequency: frequency,
             time: time,
-            minutesAway: minutesAway
-
         }
-        console.log(addedTrain);
-        database.ref().push(addedTrain);
-        console.log(currentTime);
-        //var minutesAway = moment().subtract(time).format ("hh:mm");
-        //var nextTrain = moment().add(frequency).format ("hh:mm");
-        //console.log(minutesAway);
+        
+    
+if (!name || !destination || !time || !frequency) {
+    alert("All fields are required");
 
 
+}
+
+else {
+    $("#schedule-table").empty();
+    $('#newName, #newDestination, #newTime, #newFrequency').val("");
+    
+    // console.log(addedTrain);
+    database.ref().push(addedTrain);
+}
 
     });
+    setInterval(function(){ 
+        $("#schedule-table").empty();
     database.ref().on("child_added", function (childSnapshot) {
-
+        var trainData = childSnapshot.val();
+        // console.log(trainData);
+        var convertedTime = moment(trainData.time, "HH:mm").subtract(1,"years");
+        var diffTime = moment().diff(moment(convertedTime), "minutes");
+        var timeRemaining = diffTime % trainData.frequency;
+        var minutesAway = trainData.frequency-timeRemaining;
+        var nextArrival = moment().add(minutesAway, "minutes");
+        nextArrival = moment(nextArrival).format ("HH:mm");
         // Log everything that's coming out of snapshot
-        console.log(childSnapshot.val().name);
-        console.log(childSnapshot.val().destination);
-        console.log(childSnapshot.val().time);
-        console.log(childSnapshot.val().frequency);
+        // console.log(childSnapshot.val().name);
+        // console.log(childSnapshot.val().destination);
+        // console.log(childSnapshot.val().time);
+        // console.log(childSnapshot.val().frequency);
 
         var x = $('<tr>');
-        x.html(`<th scope="row">${childSnapshot.val().name}</th><td>${childSnapshot.val().destination}</td><td>${childSnapshot.val().frequency}</td><td>${childSnapshot.val().time}</td><td>${childSnapshot.val().minutesAway}</td></td>`);
+        x.html(`
+            <th scope="row">${trainData.name}</th>
+            <td>${trainData.destination}</td>
+            <td>${trainData.frequency}</td>
+            <td>${nextArrival}</td>
+            <td>${minutesAway}</td>
+            `);
+            
         $('#schedule-table').append(x);
+
+
 
         // Handle the errors
     }, function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
     });
+}, 1000);
 })
